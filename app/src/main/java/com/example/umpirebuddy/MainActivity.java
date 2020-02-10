d6bc6912489fdbf7d51c0a502981e476a2dfa3df
Brad Starnes <bradster1029@gmail.com>
2020-02-09 21:09:57 -0600

    Updated functions for better click handlers and refactored code

--- app/src/main/java/com/example/umpirebuddy/MainActivity.java
+++ app/src/main/java/com/example/umpirebuddy/MainActivity.java
@@ -2,10 +2,10 @@ package com.example.umpirebuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.view.View;
 import android.widget.TextView;
 import android.os.Bundle;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,39 +24,75 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    protected void setBalls (View view){
+    public void checkforBalls( View view){
+
 
     }
 
-    protected void countST (View view){
-        strikeCounter++;
-        System.out.println(strikeCounter);
-        TextView strikesLabel = findViewById(R.id.strikes);
-        strikesLabel.setText("Strikes "+strikeCounter);
-    }
-
-
-    protected void countBL (View view){
-        ballCounter++;
-        TextView ballsLabel = findViewById(R.id.balls);
-        ballsLabel.setText("Balls "+ballCounter);
+    public void checkforStrikes( View view){
 
     }
 
-    protected void closeApp (View view){
-        finish();
-        System.exit(0);
+    public void countST (View view){
+        TextView strikesbutton = findViewById(R.id.strike_button);
+        strikesbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                if ( strikeCounter == 3){
+                    popUp.showAtLocation(layout, Gravity.BOTTOM, 10, 10);
+                    popUp.update(50, 50, 300, 80);
+                }
+                TextView strikesLabel = findViewById(R.id.strikes);
+                strikeCounter++;
+                System.out.println(strikeCounter);
+                strikesLabel.setText("Strikes "+strikeCounter);
+            }
+        });
     }
 
-    protected void reset (View view){
-        ballCounter = 0;
-        strikeCounter = 0;
-        System.out.println("BALLS" + ballCounter);
-        System.out.println("STRIKES" + strikeCounter);
-        TextView ballsLabel = findViewById(R.id.balls);
-        TextView strikesLabel = findViewById(R.id.strikes);
-        ballsLabel.setText("Balls "+ballCounter);
-        strikesLabel.setText("Strikes "+strikeCounter);
+
+    public void countBL (View view){
+        TextView ballButton = findViewById(R.id.ball_button);
+
+        ballButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                TextView ballLabel = findViewById(R.id.balls);
+                ballCounter++;
+                System.out.println(ballCounter);
+                ballLabel.setText("Balls "+ ballCounter);
+            }
+        });
+    }
+
+    public void closeApp (View view){
+        TextView exitbutton = findViewById(R.id.exit);
+        exitbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                finish();
+                System.exit(0);
+            }
+        });
+    }
+
+    public void reset (View view){
+        TextView resetButton = findViewById(R.id.reset);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                ballCounter = 0;
+                strikeCounter = 0;
+                TextView ballsLabel = findViewById(R.id.balls);
+                TextView strikesLabel = findViewById(R.id.strikes);
+                ballsLabel.setText("Balls "+ballCounter);
+                strikesLabel.setText("Strikes "+strikeCounter);
+            }
+        });
     }
 
 }
